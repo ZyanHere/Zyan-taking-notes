0b5c4387fd8ba2cfbc87488f5b91d30174f60d82
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-07-25 15:33:29 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import Signup from "./pages/Signup/Signup"
+import Navbar from "./components/navbar"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
