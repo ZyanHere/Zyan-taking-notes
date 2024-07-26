477f1125e80185f245fcb2e4ee8ba07c7d655b51
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-07-26 22:03:42 +0530

    login page updated

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,7 +1,90 @@
+import  { useState } from "react"
+
+import { Link, useNavigate } from "react-router-dom"
+import { validateEmail } from "../../utils/helper"
+
+
+import axios from "axios"
+import { toast } from "react-toastify"
+
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const navigate = useNavigate()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address")
+      return
+    }
+
+    if (!password) {
+      setError("Please enter the password")
+      return
+    }
+
+    setError("")
+
+    // Login API
+
+    try {
+
+
+      const res = await axios.post(
+        "http://localhost:3000/api/auth/signin",
+        { email, password },
+        { withCredentials: true }
+      )
+
+      if (res.data.success === false) {
+        toast.error(res.data.message)
+        console.log(res.data)
+      }
+
+      toast.success(res.data.message)
+      navigate("/")
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
-    <div>
-      
+    <div className="flex items-center justify-center mt-28">
+      <div className="w-96 border rounded bg-white px-7 py-10">
+        <form onSubmit={handleLogin}>
+          <h4 className="text-2xl mb-7">Login</h4>
+
+          <input
+            type="text"
+            placeholder="Email"
+            className="input-box"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          
+
+          {error && <p className="text-red-500 text-sm pb-1">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            LOGIN
+          </button>
+
+          <p className="text-sm text-center mt-4">
+            Not registered yet?{" "}
+            <Link
+              to={"/signup"}
+              className="font-medium text-[#2B85FF] underline"
+            >
+              Create an account
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   )
 }
