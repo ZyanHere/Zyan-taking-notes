6aea9b3e609d6b03710523f5f57f378aa616a75d
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-07-28 20:13:17 +0530

    home and notecard updated

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,122 @@
+import { useEffect, useState } from "react"
+import NoteCard from "../../components/Cards/NoteCard"
+import { MdAdd } from "react-icons/md"
+import Modal from "react-modal"
+import AddEditNotes from "./AddEditNotes"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import Navbar from "../../components/Navbar"
+
+
 const Home = () => {
+  const { currentUser} = useSelector(
+    (state) => state.user
+  )
+
+  const [userInfo, setUserInfo] = useState(null)
+  const [allNotes, setAllNotes] = useState([])
+
+  const [isSearch, setIsSearch] = useState(false)
+
+  const navigate = useNavigate()
+
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  })
+
+  useEffect(() => {
+    if (currentUser === null || !currentUser) {
+      navigate("/login")
+    } else {
+      setUserInfo(currentUser?.rest)
+      getAllNotes()
+    }
+  }, [])
+
+  // get all notes
+  const getAllNotes = async () => {}
+
+  const handleEdit = (noteDetails) => {
+    setOpenAddEditModal({ isShown: true, data: noteDetails, type: "edit" })
+  }
+
+  // Delete Note
+  const deleteNote = async (data) => {}
+
+  //searchnote api
+  const onSearchNote = async (query) => {}
+
+  const handleClearSearch = () => {
+    setIsSearch(false)
+    getAllNotes()
+  }
+
+  const updateIsPinned = async (noteData) => {}
+
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <Navbar
+        userInfo={userInfo}
+        onSearchNote={onSearchNote}
+        handleClearSearch={handleClearSearch}
+      />
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 max-md:m-5">
+            {allNotes.map((note, index) => (
+              <NoteCard
+                key={note._id}
+                title={note.title}
+                date={note.createdAt}
+                content={note.content}
+                tags={note.tags}
+                isPinned={note.isPinned}
+                onEdit={() => {
+                  handleEdit(note)
+                }}
+                onDelete={() => {
+                  deleteNote(note)
+                }}
+                onPinNote={() => {
+                  updateIsPinned(note)
+                }}
+              />
+            ))}
+          </div>
+      </div>
+
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-[#2B85FF] hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null })
+        }}
+      >
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-md:w-[60%] max-sm:w-[70%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNotes
+          onClose={() =>
+            setOpenAddEditModal({ isShown: false, type: "add", data: null })
+          }
+          noteData={openAddEditModal.data}
+          type={openAddEditModal.type}
+          getAllNotes={getAllNotes}
+        />
+      </Modal>
+    </>
   )
 }
 
