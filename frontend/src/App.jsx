cfd279e7d657e48a6b54182528812d79c17f61ae
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-07-29 14:03:13 +0530

    all components updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
-import { ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
 import Home from "./pages/Home/Home"
 import Login from "./pages/Login/Login"
 import Signup from "./pages/Signup/Signup"
-import Navbar from "./components/Navbar"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
