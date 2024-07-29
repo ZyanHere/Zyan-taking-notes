0238693b8b39049e5b4abf58731167733e94276f
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-07-29 14:04:15 +0530

    all components updated

--- frontend/src/components/Cards/ProfileInfo.jsx
+++ frontend/src/components/Cards/ProfileInfo.jsx
@@ -1,14 +1,15 @@
+import React from "react"
 import { getInitials } from "../../utils/helper"
 
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = ({ onLogout, userInfo }) => {
   return (
     <div className="flex items-center gap-3">
       <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
-        {getInitials("Zyan Here")}
+        {getInitials(userInfo?.username)}
       </div>
 
       <div>
-        <p className="text-sm font-medium">Zyan Here</p>
+        <p className="text-sm font-medium">{userInfo?.username}</p>
       </div>
 
       <button
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,43 +1,74 @@
-import { Link, useNavigate } from 'react-router-dom'
-import SearchBar from './SearchBar'
-import ProfileInfo from './Cards/ProfileInfo'
-import { useState } from 'react'
+import { useState } from "react"
+import SearchBar from "./SearchBar/SearchBar"
+import ProfileInfo from "./Cards/ProfileInfo"
+import { Link, useNavigate } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { toast } from "react-toastify"
+import {
+  signInSuccess,
+  signoutFailure,
+  signoutStart,
+} from "../redux/user/userSlice"
+import axios from "axios"
 
-const Navbar = () => {
-    const [searchQuery, setSearchQuery] = useState("")
+const Navbar = ({ userInfo, onSearchNote, handleClearSearch }) => {
+  const [searchQuery, setSearchQuery] = useState("")
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-    const handleSearch = () => { }
-
-    const onClearSearch = () => {
-        setSearchQuery("");
+  const handleSearch = () => {
+    if (searchQuery) {
+      onSearchNote(searchQuery)
     }
-    
-    const onLogout = () => {
-        navigate("/login")
+  }
+
+  const onClearSearch = () => {
+    setSearchQuery("")
+    handleClearSearch()
+  }
+
+  const onLogout = async () => {
+    try {
+      dispatch(signoutStart())
+
+      const res = await axios.get("http://localhost:3000/api/auth/signout", {
+        withCredentials: true,
+      })
+
+      if (res.data.success === false) {
+        dispatch(signoutFailure(res.data.message))
+        toast.error(res.data.message)
+        return
+      }
+
+      toast.success(res.data.message)
+      dispatch(signInSuccess())
+      navigate("/login")
+    } catch (error) {
+      toast.error(error.message)
+      dispatch(signoutFailure(error.message))
     }
+  }
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-    <Link to={"/"}>
-      <h2 className="text-xl font-medium text-black py-2">
-        <span className="text-slate-500">Notes</span>
-        <span className="text-slate-900">Here</span>
-      </h2>
-    </Link>
+      <Link to={"/"}>
+        <h2 className="text-xl font-medium text-black py-2">
+          <span className="text-slate-500">Good</span>
+          <span className="text-slate-900">Notes</span>
+        </h2>
+      </Link>
 
-    <SearchBar
+      <SearchBar
         value={searchQuery}
         onChange={({ target }) => setSearchQuery(target.value)}
         handleSearch={handleSearch}
         onClearSearch={onClearSearch}
       />
 
-
-    <ProfileInfo onLogout={onLogout}/>
-    
-  </div>
+      <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
+    </div>
   )
 }
 
