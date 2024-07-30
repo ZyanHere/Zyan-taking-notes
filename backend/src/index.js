a0427556ab05266e131687eb66e9f475b98e434d
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-07-30 21:51:36 +0530

    note controller updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,17 @@
 import express from "express"
-import mongoose from "mongoose"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import { dbConnection } from "./database/dbConnection.js"
+import authRouter from "./routes/auth.route.js"
+import noteRouter from "./routes/note.route.js"
 
 const app = express();
 
 
-dotenv.config({
-    path: './.env'
-})
+dotenv.config()
+
+
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true,limit: "16kb"}))
 app.use(cookieParser())
@@ -27,3 +28,19 @@ dbConnection();
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
   })
+
+app.use("/api/auth", authRouter)
+app.use("/api/note", noteRouter)
+
+
+// error handling
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Serer Error"
+  
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    })
+})
