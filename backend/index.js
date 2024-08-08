836f2a6e97cab4f5698f0c2afb7ca5028788590e
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-08-08 19:47:46 +0530

    new deploy

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 // to make input as json
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["https://notes-here.vercel.app/login"], credentials: true }));
+app.use(cors({ origin: ["https://notes-here.vercel.app"], credentials: true }));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
